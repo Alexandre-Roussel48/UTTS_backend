42f7aa79dd23615a93baf9b788507e667dc3d6b8
= <alexandre.roussel.48@gmail.com>
2024-05-14 10:39:00 +0200

    add : forge routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,7 @@ router.post('/get_forge', verifyToken, userController.getForge);
 router.post('/get_vault', verifyToken, userController.getVault);
 router.post('/drop', verifyToken, userController.drop);
 router.post('/theft', verifyToken, userController.theft);
+router.post('/forge', verifyToken, userController.forge);
 router.post('/set_last_connection', verifyToken, userController.setLastConnection);
 router.post('/inventory_card', verifyToken, userController.inventoryCard);
 router.post('/vault_card', verifyToken, userController.vaultCard);
