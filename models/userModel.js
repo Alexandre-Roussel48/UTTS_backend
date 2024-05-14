72bf61f03c00e80b1cdec4db110adbfd1f953775
= <alexandre.roussel.48@gmail.com>
2024-05-14 10:39:30 +0200

    add : model functions for forge

--- models/userModel.js
+++ models/userModel.js
@@ -275,6 +275,101 @@ async function theftCard(userId) {
     }
 }
 
+async function forgeCard(userId) {
+    try {
+        const inventories = await prisma.inventory.findMany({
+            where: {
+                user_id: userId,
+                forge: true
+            }
+        });
+        if (inventories.length == 0) {
+            return;
+        }
+        const cardIds = inventories.map(inventory => inventory.card_id);
+        const cards = [];
+        for (index in cardIds) {
+            cards.push(await prisma.card.findUnique({
+                where: {
+                    id: cardIds[index]
+                }
+            }));
+        }
+        let weight = 0;
+        for (let card of cards) {
+            if (card.rarity == "common") {weight += 1;}
+            else if (card.rarity == "uncommon") {weight += 5;}
+            else if (card.rarity == "rare") {weight += 7;}
+            else if (card.rarity == "epic") {weight += 11;}
+            else if (card.rarity == "legendary") {weight += 23;}
+        }
+        if (Math.random() < 0.7) {
+            weight /= 2;
+        }
+
+        for (let inventory of inventories) {
+            await prisma.inventory.delete({
+                where: { id: inventory.id }
+            });
+        }
+
+        // Forge a new card
+        let forge;
+        if (weight <= 1) {
+            const commonCards = await prisma.card.findMany({
+              where: {
+                rarity: 'common'
+              }
+            });
+
+            forge = commonCards.sort(() => Math.random() - 0.5).slice(0, 1)[0];
+        } else if (weight <= 5) {
+            const uncommonCards = await prisma.card.findMany({
+              where: {
+                rarity: 'uncommon'
+              }
+            });
+
+            forge = uncommonCards.sort(() => Math.random() - 0.5).slice(0, 1)[0];
+        } else if (weight <= 7) {
+            const rareCards = await prisma.card.findMany({
+              where: {
+                rarity: 'rare'
+              }
+            });
+
+            forge = rareCards.sort(() => Math.random() - 0.5).slice(0, 1)[0];
+        } else if (weight <= 11) {
+            const epicCards = await prisma.card.findMany({
+              where: {
+                rarity: 'epic'
+              }
+            });
+
+            forge = epicCards.sort(() => Math.random() - 0.5).slice(0, 1)[0];
+        } else if (weight <= 23) {
+            const legendaryCards = await prisma.card.findMany({
+              where: {
+                rarity: 'legendary'
+              }
+            });
+
+            forge = legendaryCards.sort(() => Math.random() - 0.5).slice(0, 1)[0];
+        }
+
+        await prisma.inventory.create({
+            data: {
+                user_id: userId,
+                card_id: forge.id
+            }
+        });
+
+        return forge;
+    } catch (error) {
+        throw new Error(`Error forging card: ${error.message}`);
+    }
+}
+
 async function updateLastConnection(userId) {
     try {
         await prisma.user.update({
@@ -428,6 +523,7 @@ module.exports = {
     getForge,
     dropCard,
     theftCard,
+    forgeCard,
     updateLastConnection,
     createVault,
     updateForge,
