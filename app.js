419587aa127b275df8e465a16f4d976b7a7f5c09
= <alexandre.roussel.48@gmail.com>
2024-05-16 19:05:44 +0200

    add : authCookies config

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const WebSocket = require('ws');
 const routes = require('./routes');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser());
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+
 app.use(bodyParser.json());
 
 app.use('/api', routes);
