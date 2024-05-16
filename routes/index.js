47adcdc8f9c9fdb6eda32c93870a45ce983e7039
= <alexandre.roussel.48@gmail.com>
2024-05-16 19:09:08 +0200

    add : new routes

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,12 @@ const indexController = require('../controllers/indexController');
 
 const router = express.Router();
 
+router.post('/check_connection', indexController.checkConnection);
+router.post('/set_last_connection', indexController.setLastConnection);
+router.post('/get_leaderboard', indexController.getLeaderboard);
 router.post('/register', indexController.register);
 router.post('/login', indexController.login);
+router.post('/logout', indexController.logout);
 router.use('/user', userRoutes);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,6 @@ router.post('/get_vault', verifyToken, userController.getVault);
 router.post('/drop', verifyToken, userController.drop);
 router.post('/theft', verifyToken, userController.theft);
 router.post('/forge', verifyToken, userController.forge);
-router.post('/set_last_connection', verifyToken, userController.setLastConnection);
 router.post('/inventory_card', verifyToken, userController.inventoryCard);
 router.post('/vault_card', verifyToken, userController.vaultCard);
 router.post('/forge_card', verifyToken, userController.forgeCard);
