3e57f7d393b313a01f558dc9475716f191e3d22e
= <alexandre.roussel.48@gmail.com>
2024-05-28 13:19:58 +0200

    fix websockets

--- websocket/websocketManager.js
+++ websocket/websocketManager.js
@@ -10,25 +10,55 @@ const userIdToWsMap = {};
 
 wss.on('connection', (ws, req) => {
   try {
-    const tokenPattern = new RegExp(`authToken=([^;]+)`);
-    const token = req.headers.cookie.match(tokenPattern)[1];
+    const tokenPattern = /authToken=([^;]+)/;
+    const tokenMatch = req.headers.cookie.match(tokenPattern);
+
+    if (!tokenMatch) {
+      throw new Error('Token not found in cookies');
+    }
+
+    const token = tokenMatch[1];
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
     const userId = decodedToken.user_id;
 
     userIdToWsMap[userId] = ws;
 
+    ws.isAlive = true;
+    ws.on('pong', () => {
+      ws.isAlive = true;
+    });
+
     ws.on('close', () => {
       delete userIdToWsMap[userId];
     });
+
+    ws.on('error', (error) => {
+      console.error('WebSocket error:', error);
+    });
+
   } catch (error) {
-    console.error('JWT token verification failed:', error);
+    console.error('JWT token verification failed or other error:', error);
     ws.close();
   }
 });
 
+const interval = setInterval(() => {
+  wss.clients.forEach((ws) => {
+    if (!ws.isAlive) {
+      return ws.terminate();
+    }
+    ws.isAlive = false;
+    ws.ping();
+  });
+}, 30000);
+
+wss.on('close', () => {
+  clearInterval(interval);
+});
+
 const PORT = process.env.WS_PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Websocket server running on port ${PORT}`);
 });
 
 module.exports = userIdToWsMap;
