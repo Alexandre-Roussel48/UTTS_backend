2d59a242774571d05c3ab1a18e1d610d03d4d20b
= <alexandre.roussel.48@gmail.com>
2024-05-29 18:19:48 +0200

    add : rate limiter to avoid scripting

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+const dropLimiter = rateLimit({
+  windowMs: 20 * 1000,
+  max: 1,
+  message: "Too many requests, please try again later."
+});
+
+const theftLimiter = rateLimit({
+  windowMs: 120 * 1000,
+  max: 1,
+  message: "Too many requests, please try again later."
+});
+
 router.get('/inventory', userController.getInventory);
 router.get('/forge', userController.getForge);
 router.get('/vault', userController.getVault);
-router.get('/drop', userController.drop);
-router.get('/theft', userController.theft);
+router.get('/drop', dropLimiter, userController.drop);
+router.get('/theft', theftLimiter, userController.theft);
 router.get('/data', userController.getUserData);
 router.get('/notification', userController.getThefts);
 router.put('/forge', userController.forgeCard);
